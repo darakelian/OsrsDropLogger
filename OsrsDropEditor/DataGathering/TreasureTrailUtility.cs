80a0b073b4534f95e066df93a5890f0d2f069304
Daron <dha42@drexel.edu>
2017-12-03 18:49:40 -0500

    Added tabs for the levels of clue rewards and segregated clue reward objects by level in the utility class.

--- OsrsDropEditor/DataGathering/TreasureTrailUtility.cs
+++ OsrsDropEditor/DataGathering/TreasureTrailUtility.cs
@@ -11,37 +11,65 @@ namespace OsrsDropEditor
     {
         private const string osrsWikiTreasureTrailRewardsLink = "/wiki/Treasure_Trails";
 
+        public readonly int COMMON_REWARDS = 0;
+        public readonly int EASY_REWARDS = 1;
+        public readonly int MEDIUM_REWARDS = 2;
+        public readonly int HARD_REWARDS = 3;
+        public readonly int ELITE_REWARDS = 4;
+        public readonly int MASTER_REWARDS = 5;
+
         private Browser browser;
 
         /// <summary>
-        /// Dictionary with links for the images; key is item name
+        /// Dictionary with links for the images; key is an integer representing
+        /// the level of reward and the value is a list of ClueReward objects
         /// </summary>
-        private Dictionary<string, string> treasureTrailRewardImageLinks;
+        private Dictionary<int, IEnumerable<ClueReward>> treasureTrailRewardImageLinks;
 
         public TreasureTrailUtility(Browser browser)
         {
             this.browser = browser;
-            treasureTrailRewardImageLinks = new Dictionary<string, string>();
+            treasureTrailRewardImageLinks = new Dictionary<int, IEnumerable<ClueReward>>();
         }
 
+        /// <summary>
+        /// Gets the items that are possible clue rewards. There is no good way to delineate clue level tables
+        /// so we use a temp variable to track which table we are working on.
+        /// -------------------
+        /// | Index  |  Level |
+        /// |-----------------|
+        /// | 0      | Common |
+        /// | 1      | Easy   |
+        /// | 2      | Medium |
+        /// | 3      | Hard   |
+        /// | 4      | Elite  |
+        /// | 5      | Master |
+        /// -------------------
+        /// </summary>
         public void GetTreasureTrailItems()
         {
             browser.Navigate(osrsWikiTreasureTrailRewardsLink);
             IEnumerable<XmlNode> clueRewardTables = browser.SelectNodes("//*[local-name()='table' and @class='wikitable' and .//*[local-name()='th' and contains(., 'Image')]]");
+            int index = 0;
             foreach (XmlNode clueRewardTable in clueRewardTables)
             {
                 IEnumerable<XmlNode> rewardImageLinks = browser.SelectNodes(clueRewardTable, ".//*[local-name()='img']");
-                foreach (XmlNode rewardImage in rewardImageLinks)
-                    GetRewardForImage(rewardImage);
+                treasureTrailRewardImageLinks[++index] = rewardImageLinks.Select(GetRewardForImage);
             }            
         }
 
-        private void GetRewardForImage(XmlNode rewardImage)
+        private ClueReward GetRewardForImage(XmlNode rewardImage)
         {
             string itemName = rewardImage.Attributes["alt"].Value;
             string imagePath = rewardImage.Attributes["src"]?.Value ?? rewardImage.Attributes["data-src"]?.Value;
 
-            treasureTrailRewardImageLinks[itemName] = imagePath;
+            return new ClueReward { itemName = itemName, imagePath = imagePath };
         }
+    }
+
+    public struct ClueReward
+    {
+        public string itemName { get; set; }
+        public string imagePath { get; set; }
     }
 }
--- OsrsDropEditor/Forms/AddTreasureTrailRewardForm.Designer.cs
+++ OsrsDropEditor/Forms/AddTreasureTrailRewardForm.Designer.cs
@@ -28,19 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.clueLevelSelectionTab = new System.Windows.Forms.TabControl();
+            this.commonRewardsTab = new System.Windows.Forms.TabPage();
+            this.easyRewardsTab = new System.Windows.Forms.TabPage();
+            this.mediumRewardsTab = new System.Windows.Forms.TabPage();
+            this.hardRewardsTab = new System.Windows.Forms.TabPage();
+            this.eliteRewardsTab = new System.Windows.Forms.TabPage();
+            this.masterRewardsTab = new System.Windows.Forms.TabPage();
+            this.commonListView = new System.Windows.Forms.ListView();
+            this.easyListView = new System.Windows.Forms.ListView();
+            this.mediumListView = new System.Windows.Forms.ListView();
+            this.hardListView = new System.Windows.Forms.ListView();
+            this.eliteListView = new System.Windows.Forms.ListView();
+            this.masterListView = new System.Windows.Forms.ListView();
+            this.clueLevelSelectionTab.SuspendLayout();
+            this.commonRewardsTab.SuspendLayout();
+            this.easyRewardsTab.SuspendLayout();
+            this.mediumRewardsTab.SuspendLayout();
+            this.hardRewardsTab.SuspendLayout();
+            this.eliteRewardsTab.SuspendLayout();
+            this.masterRewardsTab.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // clueLevelSelectionTab
+            // 
+            this.clueLevelSelectionTab.Controls.Add(this.commonRewardsTab);
+            this.clueLevelSelectionTab.Controls.Add(this.easyRewardsTab);
+            this.clueLevelSelectionTab.Controls.Add(this.mediumRewardsTab);
+            this.clueLevelSelectionTab.Controls.Add(this.hardRewardsTab);
+            this.clueLevelSelectionTab.Controls.Add(this.eliteRewardsTab);
+            this.clueLevelSelectionTab.Controls.Add(this.masterRewardsTab);
+            this.clueLevelSelectionTab.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.clueLevelSelectionTab.Location = new System.Drawing.Point(0, 0);
+            this.clueLevelSelectionTab.Name = "clueLevelSelectionTab";
+            this.clueLevelSelectionTab.SelectedIndex = 0;
+            this.clueLevelSelectionTab.Size = new System.Drawing.Size(1097, 814);
+            this.clueLevelSelectionTab.TabIndex = 0;
+            // 
+            // commonRewardsTab
+            // 
+            this.commonRewardsTab.Controls.Add(this.commonListView);
+            this.commonRewardsTab.Location = new System.Drawing.Point(8, 39);
+            this.commonRewardsTab.Name = "commonRewardsTab";
+            this.commonRewardsTab.Padding = new System.Windows.Forms.Padding(3);
+            this.commonRewardsTab.Size = new System.Drawing.Size(1081, 767);
+            this.commonRewardsTab.TabIndex = 0;
+            this.commonRewardsTab.Text = "Common Rewards";
+            this.commonRewardsTab.UseVisualStyleBackColor = true;
+            // 
+            // easyRewardsTab
+            // 
+            this.easyRewardsTab.Controls.Add(this.easyListView);
+            this.easyRewardsTab.Location = new System.Drawing.Point(8, 39);
+            this.easyRewardsTab.Name = "easyRewardsTab";
+            this.easyRewardsTab.Padding = new System.Windows.Forms.Padding(3);
+            this.easyRewardsTab.Size = new System.Drawing.Size(1081, 767);
+            this.easyRewardsTab.TabIndex = 1;
+            this.easyRewardsTab.Text = "Easy Rewards";
+            this.easyRewardsTab.UseVisualStyleBackColor = true;
+            // 
+            // mediumRewardsTab
+            // 
+            this.mediumRewardsTab.Controls.Add(this.mediumListView);
+            this.mediumRewardsTab.Location = new System.Drawing.Point(8, 39);
+            this.mediumRewardsTab.Name = "mediumRewardsTab";
+            this.mediumRewardsTab.Size = new System.Drawing.Size(1081, 767);
+            this.mediumRewardsTab.TabIndex = 2;
+            this.mediumRewardsTab.Text = "Medium Rewards";
+            this.mediumRewardsTab.UseVisualStyleBackColor = true;
+            // 
+            // hardRewardsTab
+            // 
+            this.hardRewardsTab.Controls.Add(this.hardListView);
+            this.hardRewardsTab.Location = new System.Drawing.Point(8, 39);
+            this.hardRewardsTab.Name = "hardRewardsTab";
+            this.hardRewardsTab.Size = new System.Drawing.Size(1081, 767);
+            this.hardRewardsTab.TabIndex = 3;
+            this.hardRewardsTab.Text = "Hard Rewards";
+            this.hardRewardsTab.UseVisualStyleBackColor = true;
+            // 
+            // eliteRewardsTab
+            // 
+            this.eliteRewardsTab.Controls.Add(this.eliteListView);
+            this.eliteRewardsTab.Location = new System.Drawing.Point(8, 39);
+            this.eliteRewardsTab.Name = "eliteRewardsTab";
+            this.eliteRewardsTab.Size = new System.Drawing.Size(1081, 767);
+            this.eliteRewardsTab.TabIndex = 4;
+            this.eliteRewardsTab.Text = "Elite Rewards";
+            this.eliteRewardsTab.UseVisualStyleBackColor = true;
+            // 
+            // masterRewardsTab
+            // 
+            this.masterRewardsTab.Controls.Add(this.masterListView);
+            this.masterRewardsTab.Location = new System.Drawing.Point(8, 39);
+            this.masterRewardsTab.Name = "masterRewardsTab";
+            this.masterRewardsTab.Size = new System.Drawing.Size(1081, 767);
+            this.masterRewardsTab.TabIndex = 5;
+            this.masterRewardsTab.Text = "Master Rewards";
+            this.masterRewardsTab.UseVisualStyleBackColor = true;
+            // 
+            // commonListView
+            // 
+            this.commonListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.commonListView.Location = new System.Drawing.Point(3, 3);
+            this.commonListView.Name = "commonListView";
+            this.commonListView.Size = new System.Drawing.Size(1075, 761);
+            this.commonListView.TabIndex = 0;
+            this.commonListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // easyListView
+            // 
+            this.easyListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.easyListView.Location = new System.Drawing.Point(3, 3);
+            this.easyListView.Name = "easyListView";
+            this.easyListView.Size = new System.Drawing.Size(1075, 761);
+            this.easyListView.TabIndex = 0;
+            this.easyListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // mediumListView
+            // 
+            this.mediumListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mediumListView.Location = new System.Drawing.Point(0, 0);
+            this.mediumListView.Name = "mediumListView";
+            this.mediumListView.Size = new System.Drawing.Size(1081, 767);
+            this.mediumListView.TabIndex = 0;
+            this.mediumListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // hardListView
+            // 
+            this.hardListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.hardListView.Location = new System.Drawing.Point(0, 0);
+            this.hardListView.Name = "hardListView";
+            this.hardListView.Size = new System.Drawing.Size(1081, 767);
+            this.hardListView.TabIndex = 0;
+            this.hardListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // eliteListView
+            // 
+            this.eliteListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.eliteListView.Location = new System.Drawing.Point(0, 0);
+            this.eliteListView.Name = "eliteListView";
+            this.eliteListView.Size = new System.Drawing.Size(1081, 767);
+            this.eliteListView.TabIndex = 0;
+            this.eliteListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // masterListView
+            // 
+            this.masterListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.masterListView.Location = new System.Drawing.Point(0, 0);
+            this.masterListView.Name = "masterListView";
+            this.masterListView.Size = new System.Drawing.Size(1081, 767);
+            this.masterListView.TabIndex = 0;
+            this.masterListView.UseCompatibleStateImageBehavior = false;
             // 
             // AddTreasureTrailRewardForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(274, 229);
+            this.ClientSize = new System.Drawing.Size(1097, 814);
+            this.Controls.Add(this.clueLevelSelectionTab);
             this.Name = "AddTreasureTrailRewardForm";
             this.Text = "Treasure Trail Reward";
+            this.clueLevelSelectionTab.ResumeLayout(false);
+            this.commonRewardsTab.ResumeLayout(false);
+            this.easyRewardsTab.ResumeLayout(false);
+            this.mediumRewardsTab.ResumeLayout(false);
+            this.hardRewardsTab.ResumeLayout(false);
+            this.eliteRewardsTab.ResumeLayout(false);
+            this.masterRewardsTab.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl clueLevelSelectionTab;
+        private System.Windows.Forms.TabPage commonRewardsTab;
+        private System.Windows.Forms.TabPage easyRewardsTab;
+        private System.Windows.Forms.TabPage mediumRewardsTab;
+        private System.Windows.Forms.TabPage hardRewardsTab;
+        private System.Windows.Forms.TabPage eliteRewardsTab;
+        private System.Windows.Forms.TabPage masterRewardsTab;
+        private System.Windows.Forms.ListView commonListView;
+        private System.Windows.Forms.ListView easyListView;
+        private System.Windows.Forms.ListView mediumListView;
+        private System.Windows.Forms.ListView hardListView;
+        private System.Windows.Forms.ListView eliteListView;
+        private System.Windows.Forms.ListView masterListView;
     }
 }
